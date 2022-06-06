c515cf1b7c9a719f503fbc2127ddb22244d9a307
wayne87140 <wayne87140@gmail.com>
2022-06-06 11:49:37 +0800

    add problem 70

---
+++ 70_ClimbingStairs.py
@@ -0,0 +1,40 @@
+# My code
+class Solution:
+    def factorial(num):
+        f_result = 1
+        for index in range(num):
+            f_result*= (num-index)
+        return f_result
+    
+    def climbStairs(self, n: int) -> int:
+        combi = 0
+        for index in range(((n-1)//2+1), n+1):
+            combi += factorial(index)//(factorial(2*index-n)*factorial(n-index))
+        return combi
+
+'''
+LeetCode
+public int climbStairs(int n) {
+    // base cases
+    if(n <= 0) return 0;
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+    
+    int one_step_before = 2;
+    int two_steps_before = 1;
+    int all_ways = 0;
+    
+    for(int i=2; i<n; i++){
+    	all_ways = one_step_before + two_steps_before;
+    	two_steps_before = one_step_before;
+        one_step_before = all_ways;
+    }
+    return all_ways;
+}
+
+def climbStairs(self, n):
+    a = b = 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+'''
